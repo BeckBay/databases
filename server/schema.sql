54b6a6a9fae1da44b5e7482fd9f6dae297330783
Aslambek Baimouradov <zandak@gmail.com>
2023-07-26 19:14:47 -0400

    testing

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,24 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE IF NOT EXISTS messages (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(9),
+  message VARCHAR(20)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE friends (
+  ID INT PRIMARY KEY AUTO_INCREMENT,
+  friendName varchar(50)
+)
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
 
